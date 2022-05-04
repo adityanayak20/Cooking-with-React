07b045a54c08952609764c7d76079d4ed04e38d1
adityanayak20 <20anayak@gmail.com>
2022-05-04 06:55:36 +0530

    class and function component in Counter APP

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import React from 'react';
+import React, {createContext, useState} from 'react';
 import Counter from './Counter';
 import CounterHooks from './CounterHooks'
 
+export const ThemeContext = createContext()
+
 export default function App() {
   console.log('Render App');
+  const [theme, setTheme] = useState('red');
   return (
-    <>
+    <ThemeContext.Provider value={{backgroundColor: theme}}>
       Counter
       <Counter initialCount = {0}/>
       CounterHooks
       <CounterHooks initialCount = {0}/>
-    </>
+      <button onClick={() => setTheme(prevTheme => {return prevTheme==='red'?'blue':'red'})}>Toggle Theme</button>
+      </ThemeContext.Provider>
   )
 }
-
-// <div>
-//       Counter
-//       <Counter initialCount = {0}/>
-//       Counter Hooks
-//       <CounterHooks initialCount = {0}/>
-// </div>
--- src/CounterHooks.js
+++ src/CounterHooks.js
@@ -1,15 +1,17 @@
-import React, {useState}  from "react";
+import React, {useState, useContext}  from "react";
+import { ThemeContext } from "./App";
 
 export default function CounterHooks({initialCount}) {
   console.log('Render Counter Hooks');
 
   const [state, setState] = useState({count: initialCount})
+  const theme = useContext(ThemeContext)
 
   return (
     <div>
-     <button onClick={() => setState({count: state.count - 1})}>-</button>
+     <button style={theme} onClick={() => setState({count: state.count - 1})}>-</button>
      <span>{state.count}</span>
-     <button onClick={() => setState(prevState => {return {count: prevState.count + 1}})}>+</button>
+     <button style={theme} onClick={() => setState(prevState => {return {count: prevState.count + 1}})}>+</button>
     </div>
   )
 } 
